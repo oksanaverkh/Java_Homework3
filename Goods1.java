f49078330096657effe9a34e456ea39ad157f477
oksanaverkh <oksana-tyryatkina@yandex.ru>
2023-02-20 17:35:46 +0300

    Task1 completed

---
+++ Goods1.java
@@ -0,0 +1,11 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Goods1 {
+    private String name;
+    private Integer price;
+    private Integer grade;
+}
